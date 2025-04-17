0f31a7147bc9768f31c6bb39200bfed2886dc514
aliirfaan <irfaan.social03@gmail.com>
2025-04-17 11:24:47 +0400

    [fix] use correct middleware

---
+++ CHANGELOG.md
@@ -0,0 +1,27 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com) and this project adheres to [Semantic Versioning](https://semver.org).
+
+## 1.0.1 - 2025-04-17
+
+### Added
+
+- Nothing
+
+### Changed
+
+- Nothing
+
+
+### Deprecated
+
+- Nothing
+
+### Removed
+
+- Nothing
+
+### Fixed
+
+- routes: payment-api use correct middleware
--- routes/api/payment/payment-api.php
+++ routes/api/payment/payment-api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use aliirfaan\CitronelCommerce\Http\Controllers\Payment\PaymentCreateController;
 use aliirfaan\CitronelCommerce\Http\Controllers\Payment\PaymentUpdateController;
 use aliirfaan\CitronelCommerce\Http\Controllers\Payment\ActorPaymentController;
+use aliirfaan\CitronelCore\Http\Middleware\CitronelCorrelationToken;
 
 Route::group([
     'prefix' => config('citronel-commerce.api_route_prefix', ''),
@@ -12,7 +13,7 @@ Route::group([
     Route::post('/{order_guid}', [PaymentCreateController::class, 'create']);
 });
 
-Route::middleware([CorrelationToken::class])->group(function () {
+Route::middleware([CitronelCorrelationToken::class])->group(function () {
     Route::put('/{gateway_merchant_transaction_no}', [PaymentUpdateController::class, 'update']);
 
     Route::get('/{actor_id}/payments', [ActorPaymentController::class, 'actorPaymentsWithOrderItems']);

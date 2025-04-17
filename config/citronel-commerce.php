529db2f0838c7941cfa728ba13ad183fcddc6f6a
aliirfaan <irfaan.social03@gmail.com>
2025-04-17 15:20:01 +0400

    [config] default prefix

--- config/citronel-commerce.php
+++ config/citronel-commerce.php
@@ -22,7 +22,7 @@
 |
 */
 return [
-    'api_route_prefix' => env('CITRONEL_COMMERCE_API_ROUTE_PREFIX', null),
+    'api_route_prefix' => env('CITRONEL_COMMERCE_API_ROUTE_PREFIX', 'orders-api'),
     'back_office_api_route_prefix' => env('CITRONEL_COMMERCE_BACK_OFFICE_API_ROUTE_PREFIX', null),
     'middleware' => [
         'api' => [

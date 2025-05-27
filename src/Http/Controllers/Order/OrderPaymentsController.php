e0ddee842f5fe3e44abe53e2e8df880d703765dd
aliirfaan <aliirfaan.freelance@gmail.com>
2025-05-27 14:05:00 +0400

    [order] order payments

---
+++ src/Http/Controllers/Order/OrderPaymentsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace aliirfaan\CitronelCommerce\Http\Controllers\Order;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use aliirfaan\LaravelSimpleApi\Http\Resources\ApiResponseCollection;
+use aliirfaan\LaravelSimpleAuditLog\Services\AuditLogService;
+use aliirfaan\LaravelSimpleAuditLog\Events\AuditLogged;
+use aliirfaan\CitronelCommerce\Services\Order\CitronelOrderService;
+
+class OrderPaymentsController extends OrderController
+{
+    public function orderPayments(Request $request, $order_guid, AuditLogService $auditService, CitronelOrderService $orderService)
+    {
+        $correlationToken = $this->helperService->getCorrelationTokenFromHeader($request);
+        $reponseHeaders = $this->helperService->setCorrelationResponseHeader($correlationToken);
+
+        $this->subProcess = $this->errorCatalogueService->getSubProcess($this->mainProcess['key'], 'order_payments');
+
+        $this->actor = $request->get('actor', null);
+
+        $this->auditData = $auditService->generatePreliminaryAuditData($request, $correlationToken, $this->actor);
+        $this->auditData['al_event_name'] = $this->subProcess['name'];
+        
+        $requestArray = $request->json()->all();
+
+        try{
+            $subProcessKey = $this->subProcess['key'];
+
+            $getOrderResponse = $orderService->getOrderByGuid($order_guid);
+            if (!$getOrderResponse['success']) {
+                $subProcessEvent = $this->errorCatalogueService->getSubProcessEvent($this->mainProcess['key'], $this->subProcess['key'], 'invalid_order');
+                $code = $this->errorCatalogueService->generateCodeFromCatalogue($this->mainProcess['key'], $subProcessKey, $subProcessEvent['key']);
+
+                $this->resultResponse = $this->apiHelperService->apiNotFoundErrorResponse($this->namespace, [], null, $this->recordNotFoundErrorCatalogue()['lang'], ['code' => $code['code']]);
+
+                $this->auditData['al_is_success'] = $this->data['success'];
+                $this->auditData['al_event_name'] = $subProcessEvent['name'];
+                $this->auditData['al_code'] = $code['code'];
+                $this->auditData['al_request'] = $order_guid;
+                $this->auditData['al_message'] = $code['status'];
+                AuditLogged::dispatch($this->auditData);
+            
+                return $this->sendApiResponse($this->resultResponse, $this->resultResponse->collection['status_code'], $reponseHeaders);
+            }
+            $order = $getOrderResponse['result'];
+
+            dd($order->payments);
+
+            $generateOrderSummaryBeforeConfirmationResponse = $orderService->generateOrderSummaryBeforeConfirmation($order);
+
+            $this->data['success'] = true;
+            $this->data['status_code'] = Response::HTTP_OK;
+            $this->data['result'] = $generateOrderSummaryBeforeConfirmationResponse['result'];
+
+            $this->resultResponse = new ApiResponseCollection($this->data);
+
+        } catch (\Exception $e) {
+            $this->resultResponse = $this->handleException($e);
+        }
+
+        return $this->sendApiResponse($this->resultResponse, $this->resultResponse->collection['status_code'], $reponseHeaders);
+    }
+}
